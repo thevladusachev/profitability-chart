48328e4039dc5cfbc2ac26a3917f75cefecdb407
Vlad Usachev <Vladuschv@yandex.ru>
2022-11-12 15:58:55 +0300

    add themes and add input

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const contArr = document.querySelector('.container-chart');
 const contDiv = document.querySelector('.container-result');
 const form = document.querySelector('#form');
 
+/**
+ * Создание div с результатом 
+ * @param {number} result - Итоговая сумма
+ */
 const renderResult = (result) => {
 	let resultHTMLElement = document.createElement('div');
 	resultHTMLElement.textContent = 'Результат: ' + result.toFixed(0);
@@ -9,6 +13,10 @@ const renderResult = (result) => {
 	contDiv.appendChild(resultHTMLElement);
 };
 
+/**
+ * Создание графика
+ * @param {number[]} arrRes 
+ */
 const renderChart = (arrRes) => {
 	const createDiv = (value, index, arr) => {
 		let chartItem = document.createElement('div');
@@ -20,6 +28,9 @@ const renderChart = (arrRes) => {
 	arrRes.forEach(createDiv);
 }
 
+/**
+ * Очистка контейнеров
+ */
 const clearContainers = () => {
 	contArr.innerHTML = '';
 	contDiv.innerHTML = '';
@@ -34,11 +45,13 @@ const getInputsValues = () => {
 	const years = document.querySelector('#years');
 	const percent = document.querySelector('#percent');
 	const sum = document.querySelector('#sum');
+	const annualReplenishment = document.querySelector('#annRepl');
 
 	return {
 		years: years.value,
 		percent: percent.value,
 		sum: sum.value,
+		annualReplenishment: annualReplenishment.value,
 	}
 }
 
@@ -50,12 +63,12 @@ const getInputsValues = () => {
  * 
  * @returns {{result: number, arrRef: number[]}} Результат
  */
-const getResult = (years, percent, sum) => {
+const getResult = (years, percent, sum, annualReplenishment) => {
 	let result = Number(sum);
 	let arrRes = [];
 
 	for (let i = 0; i < years; i++) {
-		result += percent / 100 * result;
+		result = (Number(annualReplenishment) + result) * (1 + percent / 100);
 		arrRes.push(result);
 	}
 
@@ -65,18 +78,26 @@ const getResult = (years, percent, sum) => {
 	}
 };
 
+/**
+ * Функция вычисляющая результат и выводящая график на экран
+ */
 const main = (e) => {
 	e.preventDefault()
 	clearContainers()
 
-	const { years, percent, sum } = getInputsValues()
-	const res = getResult(years, percent, sum)
+	const { years, percent, sum, annualReplenishment } = getInputsValues()
+	const res = getResult(years, percent, sum, annualReplenishment)
 
 	renderResult(res.result);
 	renderChart(res.arrRes);
-}
+};
 
 form.addEventListener('submit', main);
+
+toggleThemeBtn.onclick = () => {
+	document.body.classList.toggle('dark-theme');
+	toggleThemeBtn.innerText = document.body.classList.contains('dark-theme') ? 'Светлая тема' : 'Темная тема';
+}
 
 
 
